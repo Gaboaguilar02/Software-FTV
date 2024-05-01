e027397e0df6be51461b9ab04746071e087021a2
Gaboaguilar02 <gabochif@gmail.com>
2024-05-01 14:51:44 -0500

    inicio

--- reloj.c
+++ reloj.c
@@ -1,22 +1,28 @@
-//dado una hora en formato de HH:MM (24 horas), va a determinar el grado mas pequeño entre el minutero y el horario.
-#include <math.h> 
+// dado una hora en formato de HH:MM (24 horas), va a determinar el grado mas
+// pequeño entre el minutero y el horario.
+#include <math.h>
 #include <stdio.h>
 int main(void) {
-  int Hora=0, Minutos=0;
-  do{
-    printf("Ingrese una hora (HH:MM)");
-    scanf("%d:%d", &Hora, &Minutos);
+  int hora = 0, minutos = 0;
+  float GradosQueRecorreElMinuteroCadaMinuto = 6,
+        GradosQueRecorreElHorarioCadaMinuto = 0.5;
+  do {
+    printf("Ingrese una hora (HH:MM)\n");
+    scanf("%d:%d", &hora, &minutos);
+  } while (hora < 0 || hora > 23 || minutos < 0 || minutos > 59);
+  if (hora > 12) {
+    hora = hora - 12;
   }
-  while (Hora<0 || Hora>23 || Minutos <= -0.0001 || Minutos>59);
-  double MinutosAGrados=Minutos*6;
-  double HoraAGrados=0.5*(Hora*60+Minutos);
-  double Grados=(MinutosAGrados-HoraAGrados);
-  if (Grados<0){
-    Grados=Grados*-1;
+  double minutosAGrados = minutos * GradosQueRecorreElMinuteroCadaMinuto;
+  double horaAGrados =
+      GradosQueRecorreElHorarioCadaMinuto * minutos + hora * 30;
+  double grados = (minutosAGrados - horaAGrados);
+  if (grados < 0) {
+    grados = grados * -1;
   }
-  if (Grados>180){
-    Grados=360-Grados;
+  if (grados > 180) {
+    grados = 360 - grados;
   }
-  printf("El angulo entre las manecillas es de %.0lfº", round(Grados));
+  printf("El angulo entre las manecillas es de: %.2lfº", grados);
   return 0;
 }
